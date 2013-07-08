a3cbd0931241895cefcfa98ac4d2df7b696308e4
Simone Sacchi <zsimo30@gmail.com>
2013-07-08 18:42:07 +0000

    updated web.js

--- web.js
+++ web.js
@@ -4,11 +4,13 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 var input = '';
+var buffer = new Buffer(256);
 
 fs.readFile('index.html', function(err, data) {
   if (err) throw err;
-  input = data;
-  console.log(input);
+  buffer.write(data);	
+  input = buffer.toString();
+  //console.log(input);
 });
 
 app.get('/', function(request, response) {
