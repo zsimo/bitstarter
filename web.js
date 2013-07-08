a4b12210f334c5f14750a192e484c76f72ec2761
Simone Sacchi <zsimo30@gmail.com>
2013-07-08 17:34:27 +0000

    update web.js

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
+var input = '';
+
+fs.readFile('index.html', function(err, data) {
+  if (err) throw err;
+  input = data;
+  console.log(input);
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(input);
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
