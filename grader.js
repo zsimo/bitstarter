59abf745486b7c84378ea9a01fa157d991b2bb77
Simone Sacchi <zsimo30@gmail.com>
2013-07-09 21:16:19 +0000

    hm3_3 done

--- grader.js
+++ grader.js
@@ -15,7 +15,7 @@ References:
    - https://github.com/visionmedia/commander.js
    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
- + JSON
+nn + JSON
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
@@ -25,9 +25,12 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
+var URL_DEFAULT = 'http://secure-forest-7747.herokuapp.com';
+
 var CHECKSFILE_DEFAULT = "checks.json";
 var sys = require('util');
 var rest = require('restler');
+//console.log("simone");
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -46,15 +49,27 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
+var file = false;
+
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
+//console.log(typeof htmlfile);
+  //$ = cheerioHtmlFile(htmlfile);
+
+if(file) {
+$=cheerioHtmlFile(htmlfile);
+}
+else {
+$= cheerio.load(htmlfile);
+}
+
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for(var ii in checks) {
 	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
-    }
-    return out;
+//console.log(ii);
+  }
+  return out;
 };
 
 var clone = function(fn) {
@@ -63,22 +78,55 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-.option('-u --url <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+.option('-u, --url <html_file>', 'Path to index.html')
 	.parse(process.argv);
-if (program.url){
-rest.get(program.url).on('complete', function(data) {
-  // sys.puts(data[0].message); // auto convert to object
-console.log(data);
-});
-console.log();
+//if (program.url){
+//rest.get(program.url).on('complete', function(data) {
+ //sys.puts(data[0].message); // auto convert to object
+//console.log(data);
+//);
+//}
+
+//rest.get(program.url).on('complete', function(data) {
+    
+    //var checkJson = checkHtmlFile(program.url , program.checks);
+    //var outJson = JSON.stringify(checkJson, null, 4);
+//console.log(program.url);
+
+
+if(program.url){
+rest.get(program.url).on('complete', function(data){
+var checkJson = checkHtmlFile(data, program.checks);
+var outJson = JSON.stringify(checkJson, null, 4);
+// console.log(data);
+console.log(outJson);
+})
 }
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+else {
+file = true;
+var checkJson = checkHtmlFile(program.file, program.checks);
+var outJson = JSON.stringify(checkJson, null, 4);
+
+console.log(outJson);
+
+
+}
+
+    
+    
+//console.log(outJson);
+
+//})
+//Console.log(program.url);
+//}
+//    var checkJson = checkHtmlFile(program.file, program.checks);
+  //  var outJson = JSON.stringify(checkJson, null, 4);
+    //console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
